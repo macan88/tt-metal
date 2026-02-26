5278dd6350993083ea479c3b1572a293047acd5e
Nikola Milicevic <nmilicevic@tenstorrent.com>
2026-02-26 19:04:18 +0000

    Lower PCC for BH unet loop

--- models/experimental/stable_diffusion_xl_base/tests/pcc/test_unet_loop.py
+++ models/experimental/stable_diffusion_xl_base/tests/pcc/test_unet_loop.py
@@ -22,11 +22,12 @@ from models.experimental.stable_diffusion_xl_base.tests.test_common import (
     create_user_tensors,
 )
 from tests.ttnn.utils_for_testing import assert_with_pcc, comp_pcc
+from models.common.utility_functions import is_blackhole
 import matplotlib.pyplot as plt
 
 # TODO: test 20 instead of 10 unet iterations
 UNET_LOOP_PCC = {
-    "1024x1024": {"10": 0.93, "50": 0.92},
+    "1024x1024": {"10": 0.93, "50": 0.915 if is_blackhole() else 0.92},
     "512x512": {"10": 0.84, "50": 0.92},
 }
 
