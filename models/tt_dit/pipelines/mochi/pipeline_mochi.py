1be13334996179c82685b65f2f8a45cd231fb013
Friedrich Schöller <friedrich.schoeller@spacelike.ai>
2026-03-04 10:08:58 +0000

    fix Mochi OOM

--- models/tt_dit/pipelines/mochi/pipeline_mochi.py
+++ models/tt_dit/pipelines/mochi/pipeline_mochi.py
@@ -163,7 +163,9 @@ class MochiPipeline(DiffusionPipeline):
         self.parallel_config = parallel_config
         self.vae_parallel_config = vae_parallel_config
         self.num_links = num_links
-        self.reload_dit_model = reload_dit_model  # Only required if VAE is memory-constrained.
+        self.reload_dit_model = (
+            reload_dit_model if reload_dit_model is not None else self.mesh_device.get_num_devices() <= 8
+        )  # Only required if VAE is memory-constrained.
 
         if self.reload_dit_model and not cache.cache_dir_is_set():
             msg = (
